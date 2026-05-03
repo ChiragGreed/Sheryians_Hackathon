2c99280900a24e9ecaaf07167ea534af347c0b7a
vishalnaidu77 <vminorr@gmail.com>
2026-05-03 21:01:19 +0530

    feat: implement admin and knowledge base API service modules

--- Frontend/src/features/admin/services/admin.api.js
+++ Frontend/src/features/admin/services/admin.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:9010/api/admin",
+    baseURL: "https://sheryians-hackathon.onrender.com//api/admin",
     withCredentials: true
 });
 
