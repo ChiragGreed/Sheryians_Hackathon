66b61778fce6b63f7351f6be22ba6c0dd3eaf0db
Vishal naidu <vminorr@gmail.com>
2026-05-03 21:08:19 +0530

    Fix base URL in knowledge.api.js

--- Frontend/src/features/admin/services/knowledge.api.js
+++ Frontend/src/features/admin/services/knowledge.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://sheryians-hackathon.onrender.com//api",
+    baseURL: "https://sheryians-hackathon.onrender.com/api",
     withCredentials: true
 });
 
